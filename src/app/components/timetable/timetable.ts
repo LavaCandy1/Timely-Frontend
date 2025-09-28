8590a05831a88bff009cb7df7f08332603c6b75c
LavaCandy1 <aayush.garg.1793@gmail.com>
2025-09-28 12:58:26 +0530

    shifted timetable service into its seperate folder

--- src/app/components/timetable/timetable.ts
+++ src/app/components/timetable/timetable.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { TimetableService } from '../../services/timetable';
+import { TimetableService } from '../../services/timetable/timetable';
 import { ClassSlot } from '../../models/class-slot.model';
 
 @Component({

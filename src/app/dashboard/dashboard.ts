8634036aa3a9c6ad1ee34ebe5a9829d4e615a6bc
LavaCandy1 <aayush.garg.1793@gmail.com>
2026-03-14 21:40:40 +0530

    added requests view for mods as an alternate view for dashboard

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TimeTableComponent } from '../components/timetable/timetable';
 import { Sidebar } from '../components/sidebar/sidebar';
 import { Auth } from '../services/auth/auth';
+import { Requests } from '../components/requests/requests';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [TimeTableComponent, Sidebar],
+  imports: [TimeTableComponent, Sidebar, Requests],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
@@ -14,6 +15,12 @@ export class Dashboard {
   userRole: string | null = null;
 
   constructor(private authService: Auth) {}
+
+  currentView: 'timetable' | 'requests' = 'timetable';
+
+  changeView(event: 'timetable' | 'requests') {
+    this.currentView = event;
+  }
 
   ngOnInit() {
     this.userRole = this.authService.getRole();

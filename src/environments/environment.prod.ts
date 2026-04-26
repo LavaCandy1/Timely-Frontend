139256bdea212e2dc39c31778503dc791a605103
LavaCandy1 <aayush.garg.1793@gmail.com>
2026-04-26 22:13:35 +0530

    fix: update placeholder for batch input in search form and removal of some comments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://timely-api.lavacandy.me/api', // Your deployed backend URL
+  apiUrl: 'https://timely-api.lavacandy.me/api', 
 };
